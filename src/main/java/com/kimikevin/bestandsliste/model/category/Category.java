4a33f277907cd80a5ccc41867d98d4d5e1a12546
Kelvin Eduful <keduful@st.ug.edu.gh>
2023-07-12 14:46:50 +0000

    created List and Category

---
+++ src/main/java/com/kimikevin/bestandsliste/model/category/Category.java
@@ -0,0 +1,18 @@
+package com.kimikevin.bestandsliste.model.category;
+
+/**
+    Category for all products
+ */
+public enum Category {
+    Bakery,
+    CannedGoods,
+    Beverages,
+    Dairy,
+    BakingGoods,
+    FrozenProducts,
+    Meat,
+    FarmProduce,
+    HomeCleaners,
+    PaperGoods,
+    HomeCare
+}
---
+++ src/main/java/com/kimikevin/bestandsliste/util/List.java
@@ -0,0 +1,6 @@
+package com.kimikevin.bestandsliste.util;
+
+
+import javafx.collections.ObservableList;
+
+public abstract class List<E> implements ObservableList<E> {}
